6213b55f52c4b87b0c876c0901b09ed4a3f46b4c
Matheuslm10 <matheus_lm10@hotmail.com>
2018-05-23 18:22:40 -0400

    Mais coisa pro Firebase.

--- app/src/main/java/com/example/mathe/matchandplay/BD/ConfiguracaoFireBase.java
+++ app/src/main/java/com/example/mathe/matchandplay/BD/ConfiguracaoFireBase.java
@@ -1,4 +1,25 @@
 package com.example.mathe.matchandplay.BD;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class ConfiguracaoFireBase {
+
+    private static DatabaseReference referenciaFireBase;
+    private static FirebaseAuth autenicacao;
+
+    public static DatabaseReference getFireBase(){
+        if(referenciaFireBase == null){
+            referenciaFireBase = FirebaseDatabase.getInstance().getReference();
+        }
+        return referenciaFireBase;
+    }
+
+    public static FirebaseAuth getFirebaseAutenticacao(){
+        if(autenicacao == null){
+            autenicacao = FirebaseAuth.getInstance();
+        }
+        return autenicacao;
+    }
 }
