5cda41e5a89c4c96ebf87fe215fe4d97879dccb4
Ana Flávia <anaflaviamab@hotmail.com>
2018-05-24 23:36:34 -0400

    Feita a implementação do botão para quando o usuário ainda não contem conta.

--- app/src/main/java/com/example/mathe/matchandplay/Login.java
+++ app/src/main/java/com/example/mathe/matchandplay/Login.java
@@ -7,9 +7,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.mathe.matchandplay.BD.ConfiguracaoFireBase;
+import com.example.mathe.matchandplay.Cadastro.CadastroUsuario;
 import com.example.mathe.matchandplay.ClassesObjetos.Usuario;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -23,6 +25,7 @@ public class Login extends AppCompatActivity {
     private Button btnEntrar;
     private FirebaseAuth auteticacao;
     private Usuario usuario;
+    private TextView txtSemCadastro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +35,7 @@ public class Login extends AppCompatActivity {
         edtEmail = (EditText) findViewById(R.id.edtEmail);
         edtSenha = (EditText) findViewById(R.id.edtSenha);
         btnEntrar = (Button) findViewById(R.id.btnEntrar);
+        txtSemCadastro = (TextView) findViewById(R.id.txtSemCadastro);
 
         btnEntrar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -75,6 +79,11 @@ public class Login extends AppCompatActivity {
 
     public void abrirTelaPrincipal() {
         Intent it = new Intent(this, MainActivity.class);
+        startActivity(it);
+    }
+
+    public void irParaCadastro(View v){
+        Intent it = new Intent(this, CadastroUsuario.class);
         startActivity(it);
     }
 
