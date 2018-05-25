ed42af58fd0b01c6c732bc991aa1992cf8ef200f
Ana Flávia <anaflaviamab@hotmail.com>
2018-05-24 23:07:09 -0400

    Feito código java da classe Login.

--- app/src/main/java/com/example/mathe/matchandplay/Login.java
+++ app/src/main/java/com/example/mathe/matchandplay/Login.java
@@ -1,20 +1,82 @@
 package com.example.mathe.matchandplay;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.mathe.matchandplay.BD.ConfiguracaoFireBase;
+import com.example.mathe.matchandplay.ClassesObjetos.Usuario;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Login extends AppCompatActivity {
+
+    private EditText edtEmail;
+    private EditText edtSenha;
+    private Button btnEntrar;
+    private FirebaseAuth auteticacao;
+    private Usuario usuario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        edtEmail = (EditText) findViewById(R.id.edtEmail);
+        edtSenha = (EditText) findViewById(R.id.edtSenha);
+        btnEntrar = (Button) findViewById(R.id.btnEntrar);
+
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!edtEmail.getText().toString().equals("") &&
+                        !edtSenha.getText().toString().equals("")) {
+
+                    usuario = new Usuario();
+                    usuario.setEmail(edtEmail.getText().toString());
+                    usuario.setSenha(edtSenha.getText().toString());
+                    validarLogin();
+
+                } else {
+                    Toast.makeText(Login.this, "Preencha os campos de e-mail e senha!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
 
-    public void onClickBtnTemporario(View v){
+
+    private void validarLogin() {
+        auteticacao = ConfiguracaoFireBase.getFirebaseAutenticacao();
+        auteticacao.signInWithEmailAndPassword(usuario.getEmail(), usuario.getSenha()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if (task.isSuccessful()) {
+
+                    abrirTelaPrincipal();
+                    Toast.makeText(Login.this, "Login efetuado com sucesso!", Toast.LENGTH_SHORT).show();
+
+                } else {
+                    Toast.makeText(Login.this, "Usuário ou senha inválidos!", Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+    }
+
+
+    public void abrirTelaPrincipal() {
         Intent it = new Intent(this, MainActivity.class);
         startActivity(it);
     }
+
+
 }
