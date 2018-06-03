2fcb6f90eee395ac72b15e805213b16f95f288b9
Ana Flávia <anaflaviamab@hotmail.com>
2018-06-03 15:55:04 -0400

    Merge branch 'master' of /Users/adm/Documents/MatchAndPlay/MatchAndPlay with conflicts.

---
+++ app/src/main/java/com/example/mathe/matchandplay/Cadastro/CadastrarMeusJogos.java
@@ -0,0 +1,39 @@
+package com.example.mathe.matchandplay.Cadastro;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+
+import com.example.mathe.matchandplay.R;
+
+public class CadastrarMeusJogos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastrar_meus_jogos);
+    }
+
+    public void onCheckboxClicked(View view) {
+        // Is the view now checked?
+        boolean checked = ((CheckBox) view).isChecked();
+
+        // Check which checkbox was clicked
+        switch(view.getId()) {
+            case R.id.checkbox_meat:
+                if (checked)
+                // Put some meat on the sandwich
+            else
+                // Remove the meat
+                break;
+            case R.id.checkbox_cheese:
+                if (checked)
+                // Cheese me
+            else
+                // I'm lactose intolerant
+                break;
+            // TODO: Veggie sandwich
+        }
+    }
+}
--- app/src/main/java/com/example/mathe/matchandplay/MeusJogos.java
+++ app/src/main/java/com/example/mathe/matchandplay/MeusJogos.java
@@ -1,11 +1,15 @@
 package com.example.mathe.matchandplay;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+
+import com.example.mathe.matchandplay.Cadastro.CadastrarMeusJogos;
+import com.example.mathe.matchandplay.Cadastro.CadastroUsuario;
 
 public class MeusJogos extends AppCompatActivity {
 
@@ -24,6 +28,12 @@ public class MeusJogos extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    public void cadastrarMeusJogos (View v){
+        Intent it = new Intent(this, CadastrarMeusJogos.class);
+        startActivity(it);
+
     }
 
 }
